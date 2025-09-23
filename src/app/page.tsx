8f5d7224d587edf9739b82bf2208061dd8b89e8b
Umar-silicon <umar@sphericesolutions.com>
2025-09-24 01:55:33 +0500

    working navbar

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Navbar from "@/components/layout/Navbar";
+
+export default function Home() {
+  return (
+    <div className="w-[100%] min-h-[100vh] bg-darkblue text-white px-[10px] xsm:px-[50px] pt-[20px] max-w-[1800px] mx-auto">
+       <Navbar/>
+    </div>
+  );
+}
---
+++ src/components/layout/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='w-[100%] min-h-[80px] rounded-full bg-darkgray sticky top-[20px] shadow'>
+      
+    </div>
+  )
+}
+
+export default Navbar
